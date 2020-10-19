35167b7f932d21f8d1c8c844c18eda20288d1c67
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-10-19 20:00:59 +0200

    Add styling

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -22,8 +23,17 @@ class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(color: Colors.white),
       child: Center(
-        child: Text('Hello World'),
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+            decoration: TextDecoration.none,
+          ),
+        ),
       ),
     );
   }
