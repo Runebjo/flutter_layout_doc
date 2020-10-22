7f9b9eb8f3a8e3411282fb394baf02c0c1554623
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-10-22 22:14:56 +0200

    Add card

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,46 @@ class Images extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: _buildList(),
+        child: _buildCard(),
       ),
     );
   }
 }
+
+Widget _buildCard() => SizedBox(
+      height: 210,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('1625 Main Street',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              subtitle: Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text('(408) 555-1212',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
 
 Widget _buildList() => ListView(
       children: [
